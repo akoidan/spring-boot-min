2a6c54470e167ee52af0e9fd4853ef4a0ab60c34
akoidan <deathangel908@gmail.com>
2025-12-18 04:06:48 +0100

    DB

--- src/main/java/com/example/demo/VersionController.java
+++ src/main/java/com/example/demo/VersionController.java
@@ -7,8 +7,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class VersionController {
 
+    private final DatabaseVersionService databaseVersionService;
+
+    public VersionController(DatabaseVersionService databaseVersionService) {
+        this.databaseVersionService = databaseVersionService;
+    }
+
     @GetMapping(value = "/version", produces = MediaType.TEXT_PLAIN_VALUE)
     public String version() {
-        return "1.0.0";
+        return databaseVersionService.getPostgresVersion();
     }
 }
